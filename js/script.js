d338930d478f32d82aa596cb3e4841328924fb84
citamandiri81 <136437019+citamandiri81@users.noreply.github.com>
2023-07-08 15:53:02 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
-// ketika hamburger menu di klik
+// ketika hamburger menu diklik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
+// klik diluar sidebar
 const hamburger = document.querySelector("#hamburger-menu");
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
